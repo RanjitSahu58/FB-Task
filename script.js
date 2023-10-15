48257dcda32b52577b2933e0b4a924d454aeff67
Ranjit Sahu <ranjitsahu5558@gmail.com>
2023-10-15 18:18:57 +0530

    FB-Features

---
+++ script.js
@@ -0,0 +1,6 @@
+const settingsMenu = document.querySelector('.settings-menu');
+const darkBtn = document.getElementById("dark-btn");
+
+function settingsMenuToggle(){
+settingsMenu.classList.toggle("settings-menu-height");
+}
